6aac9d24109e7b05c2937450904684fff75d1370
Alex Gherghisan <alexghr@users.noreply.github.com>
2021-12-18 10:15:27 +0200

    ci: publish release version

--- release.config.cjs
+++ release.config.cjs
@@ -1,19 +1,23 @@
 const { repository } = require("./package.json");
 module.exports = {
-  branches: ["main"],
+  branches: [
+    { name: "main" },
+    { name: "next", channel: "next", prerelease: "next" },
+  ],
   plugins: [
     [
       "@semantic-release/commit-analyzer",
       {
         preset: "angular",
-        releaseRules: [
-          { type: "build", scope: "deps", release: "patch" },
-        ],
+        releaseRules: [{ type: "build", scope: "deps", release: "patch" }],
       },
     ],
-    ["@semantic-release/release-notes-generator", {
-      preset: "angular"
-    }],
+    [
+      "@semantic-release/release-notes-generator",
+      {
+        preset: "angular",
+      },
+    ],
     "@semantic-release/changelog",
     "@semantic-release/npm",
     "@semantic-release/git",
